6d3145d54587a0ef651e45c672abc7cf36aa3e69
Songbai Yan <colorguitar@hotmail.com>
2020-05-04 18:40:13 +0800

    add four abilities chart

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import OverviewChart from './component/OverviewChart'
 
-
 function App() {
   return (
     <div className="App">
-      <OverviewChart/>
+      <OverviewChart />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/component/FourAbilitiesChart.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Chart } from '@antv/g2';
+
+const techData = [
+    { item: 'Outstanding', count: 40, percent: 0.4 },
+    { item: 'Good', count: 21, percent: 0.21 },
+    { item: 'Competent', count: 17, percent: 0.17 },
+    { item: 'Developing', count: 13, percent: 0.13 },
+    { item: 'Mariginal', count: 9, percent: 0.09 },
+  ];
+
+export default class FourAbilitiesChart extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    componentDidMount() {
+        // Step 1: 创建 Chart 对象
+        const chart = new Chart({
+            container: 'c1',
+            autoFit: true,
+            height: 500,
+          });
+          chart.coordinate('theta', {
+            radius: 0.75,
+          });
+          
+          chart.data(techData);
+          
+          chart.scale('percent', {
+            formatter: (val) => {
+              val = val * 100 + '%';
+              return val;
+            },
+          });
+          
+          chart.tooltip({
+            showTitle: false,
+            showMarkers: false,
+          });
+          
+          chart
+            .interval()
+            .position('percent')
+            .color('item')
+            .label('percent', {
+              content: (data) => {
+                return `${data.item}: ${data.percent * 100}%`;
+              },
+            })
+            .adjust('stack');
+          
+          chart.interaction('element-active');
+          
+          chart.render();
+    }
+
+    render() {
+        return (
+            <div id="ability">
+            </div>
+        )
+    }
+}
