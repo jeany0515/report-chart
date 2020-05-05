df4539d53800ac8e1cb2d390b49e69410c5a17c2
Songbai Yan <colorguitar@hotmail.com>
2020-05-05 14:04:16 +0800

    move ability ranking data

--- src/component/AbilityRankingChart.js
+++ src/component/AbilityRankingChart.js
@@ -2,82 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Chart } from '@antv/g2';
 import { Row, Col } from 'antd';
-
-const techData = [
-    { country: '李是', population: 5 },
-    { country: '李你好', population: 13 },
-    { country: '李不是', population: 15 },
-    { country: '李看空', population: 20 },
-    { country: '李不能', population: 23 },
-    { country: '李哈哈', population: 26 },
-    { country: '李还是', population: 26 },
-    { country: '李好', population: 29 },
-    { country: '李他', population: 30 },
-    { country: '李和为', population: 34 },
-    { country: '李地方', population: 35 },
-    { country: '李七', population: 37 },
-    { country: '赵六', population: 40 },
-    { country: '王五', population: 42 },
-    { country: '张三', population: 45 },
-    { country: '李四', population: 50 },
-];
-
-const learnData = [
-    { country: '李是', population: 5 },
-    { country: '李你好', population: 13 },
-    { country: '李不是', population: 15 },
-    { country: '李看空', population: 20 },
-    { country: '李不能', population: 23 },
-    { country: '李哈哈', population: 26 },
-    { country: '李还是', population: 26 },
-    { country: '李好', population: 29 },
-    { country: '李他', population: 30 },
-    { country: '李和为', population: 34 },
-    { country: '李地方', population: 35 },
-    { country: '李七', population: 37 },
-    { country: '赵六', population: 40 },
-    { country: '王五', population: 42 },
-    { country: '张三', population: 45 },
-    { country: '李四', population: 50 },
-];
-
-const compreData = [
-    { country: '李是', population: 5 },
-    { country: '李你好', population: 13 },
-    { country: '李不是', population: 15 },
-    { country: '李看空', population: 20 },
-    { country: '李不能', population: 23 },
-    { country: '李哈哈', population: 26 },
-    { country: '李还是', population: 26 },
-    { country: '李好', population: 29 },
-    { country: '李他', population: 30 },
-    { country: '李和为', population: 34 },
-    { country: '李地方', population: 35 },
-    { country: '李七', population: 37 },
-    { country: '赵六', population: 40 },
-    { country: '王五', population: 42 },
-    { country: '张三', population: 45 },
-    { country: '李四', population: 50 },
-];
-
-const commuData = [
-    { country: '李是', population: 5 },
-    { country: '李你好', population: 13 },
-    { country: '李不是', population: 15 },
-    { country: '李看空', population: 20 },
-    { country: '李不能', population: 23 },
-    { country: '李哈哈', population: 26 },
-    { country: '李还是', population: 26 },
-    { country: '李好', population: 29 },
-    { country: '李他', population: 30 },
-    { country: '李和为', population: 34 },
-    { country: '李地方', population: 35 },
-    { country: '李七', population: 37 },
-    { country: '赵六', population: 40 },
-    { country: '王五', population: 42 },
-    { country: '张三', population: 45 },
-    { country: '李四', population: 50 },
-];
+import DataService from '../service/DataService';
 
 export default class AbilityRankingChart extends Component {
     static propTypes = {
@@ -110,10 +35,10 @@ export default class AbilityRankingChart extends Component {
     }
 
     componentDidMount() {
-        this.renderAbilityRankingChart('techRanking', techData);
-        this.renderAbilityRankingChart('learnRanking', learnData);
-        this.renderAbilityRankingChart('comprehensionRanking', compreData);
-        this.renderAbilityRankingChart('communicationRanking', commuData);
+        this.renderAbilityRankingChart('techRanking', DataService.getAbilityRankingData('tech'));
+        this.renderAbilityRankingChart('learnRanking', DataService.getAbilityRankingData('learn'));
+        this.renderAbilityRankingChart('comprehensionRanking', DataService.getAbilityRankingData('comprehension'));
+        this.renderAbilityRankingChart('communicationRanking', DataService.getAbilityRankingData('communication'));
     }
 
     render() {
--- src/service/DataService.js
+++ src/service/DataService.js
@@ -43,6 +43,86 @@ class DataService {
             { item: 'Mariginal', count: 1, percent: 0.1 },
         ];
     }
+
+    static getAbilityRankingData(category) {
+        if (category === 'tech') {
+            return [
+                { country: '李是', population: 5 },
+                { country: '李你好', population: 13 },
+                { country: '李不是', population: 15 },
+                { country: '李看空', population: 20 },
+                { country: '李不能', population: 23 },
+                { country: '李哈哈', population: 26 },
+                { country: '李还是', population: 26 },
+                { country: '李好', population: 29 },
+                { country: '李他', population: 30 },
+                { country: '李和为', population: 34 },
+                { country: '李地方', population: 35 },
+                { country: '李七', population: 37 },
+                { country: '赵六', population: 40 },
+                { country: '王五', population: 42 },
+                { country: '张三', population: 45 },
+                { country: '李四', population: 50 },
+            ];
+        } else if (category === 'learn') {
+            return [
+                { country: '李是', population: 5 },
+                { country: '李你好', population: 13 },
+                { country: '李不是', population: 15 },
+                { country: '李看空', population: 20 },
+                { country: '李不能', population: 23 },
+                { country: '李哈哈', population: 26 },
+                { country: '李还是', population: 26 },
+                { country: '李好', population: 29 },
+                { country: '李他', population: 30 },
+                { country: '李和为', population: 34 },
+                { country: '李地方', population: 35 },
+                { country: '李七', population: 37 },
+                { country: '赵六', population: 40 },
+                { country: '王五', population: 42 },
+                { country: '张三', population: 45 },
+                { country: '李四', population: 50 },
+            ];
+        } else if (category === 'comprehension') {
+
+            return [
+                { country: '李是', population: 5 },
+                { country: '李你好', population: 13 },
+                { country: '李不是', population: 15 },
+                { country: '李看空', population: 20 },
+                { country: '李不能', population: 23 },
+                { country: '李哈哈', population: 26 },
+                { country: '李还是', population: 26 },
+                { country: '李好', population: 29 },
+                { country: '李他', population: 30 },
+                { country: '李和为', population: 34 },
+                { country: '李地方', population: 35 },
+                { country: '李七', population: 37 },
+                { country: '赵六', population: 40 },
+                { country: '王五', population: 42 },
+                { country: '张三', population: 45 },
+                { country: '李四', population: 50 },
+            ];
+        }
+        return [
+            { country: '李是', population: 5 },
+            { country: '李你好', population: 13 },
+            { country: '李不是', population: 15 },
+            { country: '李看空', population: 20 },
+            { country: '李不能', population: 23 },
+            { country: '李哈哈', population: 26 },
+            { country: '李还是', population: 26 },
+            { country: '李好', population: 29 },
+            { country: '李他', population: 30 },
+            { country: '李和为', population: 34 },
+            { country: '李地方', population: 35 },
+            { country: '李七', population: 37 },
+            { country: '赵六', population: 40 },
+            { country: '王五', population: 42 },
+            { country: '张三', population: 45 },
+            { country: '李四', population: 50 },
+        ];
+    }
 }
 
 export default DataService
