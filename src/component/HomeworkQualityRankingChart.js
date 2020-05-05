59418ac5ed5b711c7a407ed3409617a7af2ac1af
Songbai Yan <colorguitar@hotmail.com>
2020-05-05 14:06:32 +0800

    move homework quality ranking data

--- src/component/HomeworkQualityRankingChart.js
+++ src/component/HomeworkQualityRankingChart.js
@@ -1,25 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Chart } from '@antv/g2';
-
-const data = [
-    { country: '李是', population: 5 },
-    { country: '李你好', population: 13 },
-    { country: '李不是', population: 15 },
-    { country: '李看空', population: 20 },
-    { country: '李不能', population: 23 },
-    { country: '李哈哈', population: 26 },
-    { country: '李还是', population: 26 },
-    { country: '李好', population: 29 },
-    { country: '李他', population: 30 },
-    { country: '李和为', population: 34 },
-    { country: '李地方', population: 35 },
-    { country: '李七', population: 37 },
-    { country: '赵六', population: 40 },
-    { country: '王五', population: 42 },
-    { country: '张三', population: 45 },
-    { country: '李四', population: 50 },
-];
+import DataService from '../service/DataService';
 
 export default class HomeworkQualityRankingChart extends Component {
     static propTypes = {
@@ -33,7 +15,7 @@ export default class HomeworkQualityRankingChart extends Component {
             height: 500,
         });
 
-        chart.data(data);
+        chart.data(DataService.getHomeworkQualityRankingData());
         chart.scale('population', { nice: true });
         chart.coordinate().transpose();
         chart.tooltip({
--- src/service/DataService.js
+++ src/service/DataService.js
@@ -144,6 +144,27 @@ class DataService {
             { country: '张三', population: 1320 },
         ];
     }
+
+    static getHomeworkQualityRankingData() {
+        return [
+            { country: '李是', population: 5 },
+            { country: '李你好', population: 13 },
+            { country: '李不是', population: 15 },
+            { country: '李看空', population: 20 },
+            { country: '李不能', population: 23 },
+            { country: '李哈哈', population: 26 },
+            { country: '李还是', population: 26 },
+            { country: '李好', population: 29 },
+            { country: '李他', population: 30 },
+            { country: '李和为', population: 34 },
+            { country: '李地方', population: 35 },
+            { country: '李七', population: 37 },
+            { country: '赵六', population: 40 },
+            { country: '王五', population: 42 },
+            { country: '张三', population: 45 },
+            { country: '李四', population: 50 },
+        ];
+    }
 }
 
 export default DataService
