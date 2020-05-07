87c280eab7613a684f7aa10c0f60102b4d939efc
Songbai Yan <colorguitar@hotmail.com>
2020-05-07 17:14:28 +0800

    use real data for all ranking

--- src/service/DataService.js
+++ src/service/DataService.js
@@ -134,24 +134,11 @@ class DataService {
     }
 
     static getAllRankingData() {
-        return [
-            { country: '李是', population: 650 },
-            { country: '李你好', population: 699 },
-            { country: '李不是', population: 700 },
-            { country: '李看空', population: 732 },
-            { country: '李不能', population: 750 },
-            { country: '李哈哈', population: 789 },
-            { country: '李还是', population: 799 },
-            { country: '李好', population: 821 },
-            { country: '李他', population: 840 },
-            { country: '李是啊', population: 870 },
-            { country: '李地方', population: 900 },
-            { country: '李七', population: 902 },
-            { country: '赵六', population: 1000 },
-            { country: '王五', population: 1100 },
-            { country: '李四', population: 1220 },
-            { country: '张三', population: 1320 },
-        ];
+        const totalScores = data.总分.map(trainee => {
+            return { name: trainee.名字, score: parseInt(trainee.总分) }
+        })
+        totalScores.sort(function (a, b) { return a.score - b.score })
+        return totalScores
     }
 
     static getHomeworkQualityRankingData() {
